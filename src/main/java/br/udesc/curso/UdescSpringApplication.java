10c7220d3cfffaa48c17a5fb2e050cf0d570cb38
Matheus Valenza <matheusvvalenza@gmail.com>
2018-07-30 20:23:59 -0300

    initial commit

---
+++ src/main/java/br/udesc/curso/UdescSpringApplication.java
@@ -0,0 +1,12 @@
+package br.udesc.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdescSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdescSpringApplication.class, args);
+	}
+}
---
+++ src/main/java/br/udesc/curso/controller/OlaMundoController.java
@@ -0,0 +1,21 @@
+package br.udesc.curso.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class OlaMundoController {
+
+	@GetMapping("/ola")
+	public ResponseEntity<String> getOlaMundo() {
+		return ResponseEntity.ok("Olá, UDESC!");
+	}
+	
+	@GetMapping("/comoVaiVoce")
+	public ResponseEntity<String> getComoVaiVoce() {		
+		return ResponseEntity.ok("Como vai você?");
+	}
+}
